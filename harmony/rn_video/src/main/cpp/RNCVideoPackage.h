c857130a25472b13bd3ed369fa26c37bafa98bec
liulihong <llh_01129@163.com>
2024-03-18 12:41:49 +0800

    video混合适配修改
    Signed-off-by: liulihong <llh_01129@163.com>

--- harmony/rn_video/src/main/cpp/RNCVideoPackage.h
+++ harmony/rn_video/src/main/cpp/RNCVideoPackage.h
@@ -37,9 +37,9 @@ class RNCVideoComponentInstanceFactoryDelegate : public ComponentInstanceFactory
 public:
     using ComponentInstanceFactoryDelegate::ComponentInstanceFactoryDelegate;
 
-    ComponentInstance::Shared create(ComponentInstanceFactoryContext ctx) override {
+    ComponentInstance::Shared create(ComponentInstance::Context ctx) override {
         if (ctx.componentName == "RNCVideo") {
-            return std::make_shared<ArkTSComponentInstance>(m_ctx, ctx.tag);
+            return std::make_shared<ArkTSComponentInstance>(ctx);
         }
         return nullptr;
     }
